7a176a6ebb704cebda7ea8a8676f773a1dc6d44a
Wang Zuo <wzuoadjusted@gmail.com>
2018-07-25 14:02:49 +0800

    update deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,10 @@
 module.exports = {
+  mode: 'development',
   entry: './index.js',
   output: {
     path: __dirname + '/dist',
     filename: 'bloodhound.min.js',
     library: 'Bloodhound'
   },
-  module: {
-    loaders: [
-      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
-    ]
-  },
-  devtool: "source-map"
+  devtool: 'source-map'
 };
